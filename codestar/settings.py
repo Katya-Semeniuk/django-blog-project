e6650b3e2eed416373b348ed2eeac662f36f5ec7
Kateryna Semeniuk <katyasemeniuk@gmail.com>
2024-07-13 18:15:53 +0000

    Admin panel power-up

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-katyasemeni-djangoblogp-ew70paykqk5.ws.codeinstitute-ide.net", '.herokuapp.com']
 
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
+    
 ]
 
 MIDDLEWARE = [
